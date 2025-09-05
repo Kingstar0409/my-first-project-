c210b7b4e44d5af3515ef9f4fcc7304a7c6bdfd0
Kingstar <debashisdash0409@gmail.com>
2025-09-05 20:16:47 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator{
+   
+    public static void main(String args[]){
+         int FirstNum=5;    
+         int SecondNum=2;
+         int Sum= FirstNum + SecondNum; 
+        System.out.println("Sum is "+Sum);
+    }
+}
